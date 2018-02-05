56ff4b514ff17444d990132d84e81cf140888580
Nirmalvekariya <34475292+Nirmalvekariya@users.noreply.github.com>
2018-02-05 17:54:58 +0530

    simple twitterbots
    simple twitter bots

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # twitterbots and webscraping
+
+
+This code will require bs4,tweepy and request libraries.
+
+Get your APi keys from twitter and paste it in the above code.By running the code it will automatically tweet the leading news from ZEE NEWs website
+
---
+++ tweet_it.py
@@ -0,0 +1,28 @@
+from tweepy  import OAuthHandler, API
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+# Credentials to access Twitter API
+ACCESS_TOKEN    = 'paste your access token here'
+ACCESS_SECRET   = 'paste your access secret here'
+CONSUMER_KEY    = 'paste your consumer key here'
+CONSUMER_SECRET = 'paste your consumer secret here'
+
+
+Auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+Auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+TwitterBot = API(Auth)
+
+#getting cotents from website
+r=requests.get("http://zeenews.india.com/")
+c=r.content
+
+soup=BeautifulSoup(c,"html.parser")
+
+
+all=soup.find_all("div",{"class":"lead-head"})
+for item in all:
+    TwitterBot.update_status(item.find_all("h1")[0].text+' '+"#zeenews"+"#newsofday")
